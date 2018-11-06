7eafa49c830b3ab21bb0f352a4e6496eb482bad3
wyq <baohudongwu@qq.com>
2018-11-06 15:13:04 +0800

    use pro daily add get_pro_daily method

--- pro_hq.py
+++ pro_hq.py
@@ -34,4 +34,53 @@ def toDB_pro_dividend():
             df.to_sql('t_pro_dividend', c.ENGINE, if_exists='append')
     print("toDB_pro_dividend end " + str(datetime.datetime.now()))
 
-toDB_pro_dividend()
+#处理5,10,20,30,60,120日线，量比 pro接口
+def get_pro_kline():
+    start = datetime.datetime.now()
+    print(start)
+    hq._excutesql("delete from t_kline")
+    sql_body = "SELECT trade_date,ts_code,close,vol FROM t_pro_daily WHERE  ts_code = '"
+    sql_foot = "' and trade_date <= '"+c.DATE.replace('-','')+"' order by trade_date desc limit 120"
+    sql_u = "update t_pro_daily a,t_kline b set a.vr=b.vr,a.ma5=b.ma5,a.ma10=b.ma10,a.ma20=b.ma20,a.ma30=b.ma30,a.ma60=b.ma60,a.ma120=b.ma120 where a.trade_date =b.datetime and a.ts_code =b.code"
+    def calc(market,sql):
+        if(market == 'SH'):
+            sql = "select code from T_StockBasics where code like '6%%' order by code "
+        if (market == 'SZ'):
+            sql = "select code from T_StockBasics where code like '0%%' order by code "
+        if (market == 'CYB'):
+            sql = "select code from T_StockBasics where code like '3%%' order by code "
+        for row in hq._excutesql(sql):
+            print(row['code'])
+            sql = sql_body + row['code'] + sql_foot
+            print(sql)
+            df = pd.DataFrame(hq._excutesql(sql).fetchall())
+            if (df.empty):
+                print('empty')
+                continue
+            else:
+                df.columns = ['datetime', 'code', 'close', 'vol']
+                df['vol5'] = tsu.MA(df['vol'], 5)
+                df['mean'] = df['vol5'].shift(-5)
+                df['vr'] = df['vol'] / df['mean']
+                df['vr'] = df['vr'].map(c.FORMAT)
+                df['vr'] = df['vr'].astype(float)
+                for a in c.MA:
+                    if isinstance(a, int):
+                        df['ma%s' % a] = tsu.MA(df['close'], a).map(c.FORMAT).shift(-(a - 1))
+                df1 = df.drop(['close','vol','vol5','mean'], axis=1)
+                df1.ix[:0].to_sql('t_kline', c.ENGINE, if_exists='append')
+    try:
+        t_sh = threading.Thread(target=calc, args=("SH", "sql"))
+        t_sz = threading.Thread(target=calc, args=("SZ", "sql"))
+        t_cyb = threading.Thread(target=calc, args=("CYB", "sql"))
+        for t in [t_sh, t_sz, t_cyb]:
+            t.start()
+        for t in [t_sh, t_sz, t_cyb]:
+            t.join()
+    except Exception as e:
+        print(e)
+    print("############### UPDATE STARTING ###############")
+    hq._excutesql(sql_u)
+    end = datetime.datetime.now()
+    print("get_pro_kline: " + str(end - start))
+
