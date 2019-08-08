5725c36f83e9882d526924919a29029a555ae225
wyq <baohudongwu@qq.com>
2019-08-08 08:41:23 +0800

    renew main.py

--- main.py
+++ main.py
@@ -12,34 +12,75 @@ import newUtil as nu
 import kline as kl
 import threading
 import tushare.util.formula as tsu
+import pro_hq as pro_hq
+import importEntrust as ith
+import time
+
+# print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+# hq.get_com()
+# print("1")
+# hq.toDB_record('get_shrink',u.get_shrink(0),"DAY="+str(c.DAYS)+",SHRINK_P=1.0101")
+# print("2")
+# uod='u'
+# hq.toDB_record('get_soared',u.get_soared(0,uod),"uod ="+uod+",day="+str(c.DAYS)+",m5_10 ="+ str(c.m5_10)+",m10_20 = "+str(c.m10_20)+",TURNOVER="+str(c.TURNOVER)+",VOL="+str(c.VOL)+",UPSH="+str(c.UPSD_RATIO))
+# print("3")
+# d = {"5":"10","10":"15", "15":"20","20":"25","25":"30","30":"9999"}
+# for key,value in d.items():
+#     hq.toDB_record('get_aoi', u.get_aoi(2, top=value, low=key),"low=" + key+",top=" + value+",day="+str(c.DAYS))
+# print("4")
+# uod='d'
+# if(u.get_soared(0,uod) != 'NULL'):
+#     hq.toDB_record('get_soared',u.get_soared(0,uod),"uod ="+uod+",day="+str(c.DAYS)+",m5_10 ="+ str(c.m5_10)+",m10_20 = "+str(c.m10_20)+",TURNOVER="+str(c.TURNOVER)+",VOL="+str(c.VOL)+",DWSH="+str(c.DOWNSD_RATIO))
+# print("5")
+# if(u.get_hopeDOJI()!='NULL'):
+#     hq.toDB_record('get_hopeDOJI', u.get_hopeDOJI(), "DOJI=" + str(c.DOJI))
+#
+# start = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+# pro_hq.toDB_pro_common()
+# pro_hq.get_pro_kline()
+# end = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+# print('START   '+start)
+# print('END   '+end)
+
+ith.importcsv_excle('20190807')
+#
+# df = pd.read_excel("C:/Users/wyq/Desktop/123.xlsx",sheet_name='123')
+# df.to_sql('t_stockentrust', c.ENGINE, if_exists='append')
 
 
-#hq.get_com()
-print("1")
-hq.toDB_record('get_shrink',u.get_shrink(0),"DAY="+str(c.DAYS)+",SHRINK_P=1.0101")
-print("2")
-uod='u'
-hq.toDB_record('get_soared',u.get_soared(0,uod),"uod ="+uod+",day="+str(c.DAYS)+",m5_10 ="+ str(c.m5_10)+",m10_20 = "+str(c.m10_20)+",TURNOVER="+str(c.TURNOVER)+",VOL="+str(c.VOL)+",UPSH="+str(c.UPSD_RATIO))
-print("3")
-d = {"5":"10","10":"15", "15":"20","20":"25","25":"30","30":"9999"}
-for key,value in d.items():
-    hq.toDB_record('get_aoi', u.get_aoi(2, top=value, low=key),"low=" + key+",top=" + value+",day="+str(c.DAYS))
-print("4")
-uod='d'
-if(u.get_soared(0,uod) != 'NULL'):
-    hq.toDB_record('get_soared',u.get_soared(0,uod),"uod ="+uod+",day="+str(c.DAYS)+",m5_10 ="+ str(c.m5_10)+",m10_20 = "+str(c.m10_20)+",TURNOVER="+str(c.TURNOVER)+",VOL="+str(c.VOL)+",DWSH="+str(c.DOWNSD_RATIO))
-print("5")
-if(u.get_hopeDOJI()!='NULL'):
-    hq.toDB_record('get_hopeDOJI', u.get_hopeDOJI(), "DOJI=" + str(c.DOJI))
-
-# conn = ts.get_apis()
-# #df=ts.bar('000001', conn=conn, freq='D', start_date=c.DATE, end_date=c.DATE,adj='qfq', ma=[5, 10, 20, 30,60,120],factors=['tor', 'vr'])
-# df = ts.bar('000560', conn=conn, freq='D', start_date=c.DATE, end_date=c.DATE,adj='qfq')
-# # df = ts.bar(code = '10001405',conn=conn,asset='X')
+# ts.set_token('37dd55b1cdf9e94548a9731821cdaf49c0d04be8ac6d038877a7341e')
+# pro = ts.pro_api()
+# #df = pro.index_weight(index_code='000016.SH', end_date='20191101')
+# df = pro.concept()
+# # df['ts_code'] = df['ts_code'].map(c.PRO_CODE_FORMAT)
+# # df.to_sql('t_pro_optbasic', c.ENGINE, if_exists='append')
 # print(df)
-# ts.close_apis(conn)
 
-# hq.toDB_get_periodata('D')
-# hq.get_kline('D')
 
-print(u.get_hopeDOJI())
+# ts.set_token('37dd55b1cdf9e94548a9731821cdaf49c0d04be8ac6d038877a7341e')
+# pro = ts.pro_api()
+# df = pro.daily(trade_date='20190513')
+# df.to_sql('t_pro_optdaily', c.ENGINE, if_exists='append')
+# print(df)
+
+
+
+#df.to_sql('t_pro_fundbasic', c.ENGINE, if_exists='append')
+
+#计算时间间隔
+# def get_lasttradeday(id,date,days):
+#     SQL_LASTTRADEDAY = "SELECT cal_date from t_pro_tradeday where  is_open = 1 and cal_date >'" + date + "'"
+#     print(SQL_LASTTRADEDAY)
+#     df = pd.DataFrame(hq._excutesql(SQL_LASTTRADEDAY).fetchall())
+#     df = df.reset_index()
+#     print(df)
+#     SQL = "update tmp set newdate ='" + df[0][days-1] + "' WHERE id = "+ str(id)
+#     print(SQL)
+#     #hq._excutesql(SQL)
+#
+# for id,date,days in hq._excutesql("select id,date,days from tmp order by id"):
+#     try:
+#         get_lasttradeday(id,date,days)
+#     except:
+#         continue
+
